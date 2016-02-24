421b73f98c7a206bedf02ddf694effadaed156be
Christian Nwamba <nwambachristian@gmail.com>
2016-02-24 16:24:00 +0100

    With queues

--- app/Listeners/SendNewsletter.php
+++ app/Listeners/SendNewsletter.php
@@ -8,7 +8,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Facades\Mail;
 
-class SendNewsletter
+class SendNewsletter implements ShouldQueue
 {
     /**
      * Create the event listener.
